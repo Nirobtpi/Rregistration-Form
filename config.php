73b8c737fd127f0389b2c7ebc0463431afc922dc
Nirobtpi <saifasabanirob@gmail.com>
2024-01-31 23:55:43 +0600

    create connection

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+
+$hostname = "localhost";
+$userName = 'root';
+$dbName = "test";
+$password = "";
+
+try {
+    $conn = new PDO("mysql:host=$hostname;dbname=$dbName", $userName, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    // echo "Connection Success";
+} catch (PDOException $e) {
+    echo "Connection Fail " . $e->getMessage();
+}
